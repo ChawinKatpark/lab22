49f360db9a5ca79f798c083eba86fa665a75f0ca
ChawinKatpark <141801767+ChawinKatpark@users.noreply.github.com>
2024-02-27 00:24:46 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc,char** argv){
+	int num = 0;
+	if(argc == 1){
+	cout << "Please input numbers to find average.";
+	}
+	else{ for(int i = 1 ;i<=(argc-1);i++){
+		num += atoi(argv[i]);
+		}
+		float avg = num/(argc-1);
+		cout << "---------------------------------\n";
+		cout << "Average of " << (argc-1) << " numbers = " << avg;
+		cout << "\n---------------------------------";
+		
+	}
+}
